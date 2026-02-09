1e6ee737f7d74910de0a59b6921b878161995d71
JoaoGeraldo-dev <joaogeraldobussolo@gmail.com>
2026-02-09 11:07:57 -0300

    09/02

---
+++ Exemplo1/src/exemplo1/Exemplo1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo1;
+
+/**
+ 
+ * @author aluno
+ */
+public class Exemplo1 {    
+    public static void main(String[] args) {
+        System.out.println("Ola mudo");
+   
+    int idade = 18;
+    double altura = 1.72;
+    String nome = "João Geraldo";
+    
+     System.out.println("Ola" +nome+ "seja bem vindo");
+     System.out.println("Sua altura é " + altura);
+     
+     double dias = idade * 365;
+        System.out.println("Sua idade é " + idade);
+        System.out.println("voce ja viveu " + dias + " dias");
+             }
+}
